ecbaeed74344fbbebef0a352af7b2e633d417c3b
Jhoans Aaron <onichanbb1@gmail.com>
2025-06-30 16:39:27 -0500

    Usando base de datos

---
+++ miBackend/server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Conectar a MongoDB
+mongoose.connect('mongodb://localhost:27017/Cinesario', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// Esquema de ejemplo
+const Usuario = mongoose.model('Usuario', {
+  nombre: String,
+  email: String,
+});
+
+// Ruta para obtener usuarios
+app.get('/usuarios', async (req, res) => {
+  const usuarios = await Usuario.find();
+  res.json(usuarios);
+});
+
+// Iniciar servidor
+app.listen(3000, () => {
+  console.log('Servidor corriendo en http://localhost:27017');
+});
