46974493c41b4381328c6650dd2538162ba53998
Jhoans Aaron <onichanbb1@gmail.com>
2025-07-07 13:23:56 -0500

    Asegurando que este funcionado el servidor

--- server.js
+++ server.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Conectar a MongoDB
-mongoose.connect('mongodb://localhost:27017/Cinesario', {
+// Conectar a MongoDB Atlas (usa tu MONGO_URI del .env)
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-});
+})
+  .then(() => console.log('Conexión a MongoDB Atlas exitosa'))
+  .catch(err => console.error('Error de conexión a MongoDB:', err));
 
 // Esquema de ejemplo
 const Usuario = mongoose.model('Usuario', {
@@ -26,12 +29,5 @@ app.get('/usuarios', async (req, res) => {
 
 // Iniciar servidor
 app.listen(3000, () => {
-  console.log('Servidor corriendo en http://localhost:27017');
+  console.log('Servidor corriendo en http://localhost:3000');
 });
-
-require('dotenv').config();
-const mongoose = require('mongoose');
-
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log('Conexión a MongoDB Atlas exitosa'))
-  .catch(err => console.error(err));
